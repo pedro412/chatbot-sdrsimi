29b82bb628381465ca2257dd5858549f0646ecea
Alvarez Perez Pedro <pedro.alvarez412@outlook.com>
2019-06-07 16:45:30 -0500

    Dev enviroment setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
+const bodyParser = require('body-parser');
 
 const app = express();
-const { PORT = 3000 } = process.env;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const { PORT = 5000 } = process.env;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send({
-    message: 'Up and Running 🚀'
-  });
+	res.send({
+		status: 'Up 🚀',
+	});
 });
 
 app.listen(PORT, () => {
-  console.log('server started at http://localhost:' + PORT);
+	console.log(`server started at http://localhost: ${PORT}`);
+});
+
+app.get('/webhook', (req, res) => {
+	if (req.query['hub.verify_token'] === 'simi2019') {
+		res.send(req.query['hub.challenge']);
+	} else {
+		res.send({
+			message: 'Unauthorized',
+		});
+	}
 });
